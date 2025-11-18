4e64118549201b5ee58c7b9949101035e4de32d9
William <williamverde10@gmail.com>
2025-11-18 17:30:58 -0400

    Added: feature other price with validations | only full products value in the car

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Install
 ```bash
-composer require imagina/icommerceordertotal-module=v8.x-dev
+composer require imagina/icommerceordertotal-module=v10.x-dev
 ```
 
 ## Enable the module
@@ -14,3 +14,21 @@ php artisan module:enable Icommerceordertotal
 ```bash
 php artisan module:seed Icommerceordertotal
 ```
+
+## Configuration Iadmin Example:
+
+```bash
+//Case - From - Mayores a
+{
+  "to": 300000,
+  "value": 15000
+},
+//Case - to - Menores a 
+{
+  "from": 300000,
+  "value": 15000,
+  "onlyFullProductsValue": 0 
+},
+```
+
+"onlyFullProductsValue" => 'Si en los productos que estan en el carrito existe al menos uno sin descuento, se aplica el valor de "onlyFullProductsValue" como el valor de envio
--- Repositories/Eloquent/EloquentIcommerceOrdertotalRepository.php
+++ Repositories/Eloquent/EloquentIcommerceOrdertotalRepository.php
@@ -8,47 +8,84 @@ use Modules\Core\Repositories\Eloquent\EloquentBaseRepository;
 class EloquentIcommerceOrdertotalRepository extends EloquentBaseRepository implements IcommerceOrdertotalRepository
 {
 
-    function calculate($parameters,$conf){
-         
+    function calculate($parameters, $conf)
+    {
+
+        //\Log::info("Icommerceordertotal | Calculate");
+
         $orderTotal = $parameters['products']['total'];
         $orderValuesRanges = $conf->orderTotalRanges;
+
+        //Check Products
+        $items = json_decode($parameters["products"]["items"]);
+
+        //Validation Products | onlyFullProductsValue
+        $oneFullPrice = $this->checkOneProductoWithoutDiscount($items);
 
         $shippingValue = 0;
         //Check each range
         foreach ($orderValuesRanges as $key => $range) {
 
-            //\Log::info("Range: ".json_encode($range));
-        
+            //\Log::info("Range: " . json_encode($range));
+
             //Case - Between - Desde xxx Hasta xxx
-            if(isset($range->from) && isset($range->to)){
-                if($orderTotal>=$range->from && $orderTotal<=$range->to){
-                     $shippingValue = $range->value; 
-                     break;
-                }
-            }else{
-                //Case - From - Mayores a
-                if(isset($range->from) && $orderTotal>$range->from){
+            if (isset($range->from) && isset($range->to)) {
+                if ($orderTotal >= $range->from && $orderTotal <= $range->to) {
                     $shippingValue = $range->value;
-                }else{
+                    break;
+                }
+            } else {
+                //Case - From - Mayores a
+                if (isset($range->from) && $orderTotal > $range->from) {
+                    //\Log::info("Range: Aplica From");
+                    $shippingValue = $range->value;
+                    if (isset($range->onlyFullProductsValue) && $oneFullPrice) {
+                        $shippingValue = $range->onlyFullProductsValue;
+                    }
+                    break;
+                } else {
                     //Case - to - Menores a 
-                    if(isset($range->to) && $orderTotal<$range->to){
+                    if (isset($range->to) && $orderTotal < $range->to) {
+                        //\Log::info("Range: Aplica To");
                         $shippingValue = $range->value;
+                        if (isset($range->onlyFullProductsValue) && $oneFullPrice) {
+                            $shippingValue = $range->onlyFullProductsValue;
+                        }
+                        break;
                     }
                 }
-            }   
-              
+            }
         }
 
         //============= Standart Response
         $response["status"] = "success";
-        $response["items"] = null;    
+        $response["items"] = null;
 
         //============= Final Price
         $response["price"] = $shippingValue;
         $response["priceshow"] = true;
 
         return $response;
-
     }
 
+    function checkOneProductoWithoutDiscount($items)
+    {
+
+        //Get all products information
+        $productIds = [];
+        foreach ($items as $item) {
+            $productIds[] = $item->product_id;
+        }
+        $products = \Modules\Icommerce\Entities\Product::whereIn('id', $productIds)->get();
+
+        //validation discount product
+        foreach ($products as $key => $product) {
+            $discount =  $product->discount;
+            if (is_null($discount))
+                return true; //Not discount for this cart
+        }
+
+
+        return false;
+    }
 }
